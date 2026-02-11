84e8259e1102e55821615d6f0d248ee4146e99b4
alina <connorfromcyberlife28@mail.ru>
2026-02-11 18:08:40 +0300

    build7

---
+++ docs/buttons.js
@@ -0,0 +1,9 @@
+/******/ (() => { // webpackBootstrap
+document.querySelector('.cat-cont1').addEventListener('click', function () {
+  window.location.href = 'pages/hello.html';
+});
+document.querySelector('.cat-cont2').addEventListener('click', function () {
+  window.location.href = 'hearts.html';
+});
+/******/ })()
+;
