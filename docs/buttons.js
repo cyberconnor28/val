81058238a3dcce8eeb1ed2590cdcf59065aa9e7d
alina <connorfromcyberlife28@mail.ru>
2026-02-12 18:00:10 +0300

    meowmeow

--- docs/buttons.js
+++ docs/buttons.js
@@ -1,9 +1,9 @@
 /******/ (() => { // webpackBootstrap
-document.querySelector('.cat-cont1').addEventListener('click', function () {
-  window.location.href = 'pages/hello.html';
-});
 document.querySelector('.cat-cont2').addEventListener('click', function () {
   window.location.href = 'hearts.html';
+});
+document.querySelector('.heart3-3').addEventListener('click', function () {
+  window.location.href = 'time.html';
 });
 /******/ })()
 ;
--- docs/index.js
+++ docs/index.js
@@ -1,5 +1,8 @@
 /******/ (() => { // webpackBootstrap
 /******/ 	"use strict";
 
+document.querySelector('.cat-cont1').addEventListener('click', function () {
+  window.location.href = 'pages/hello.html';
+});
 /******/ })()
 ;
