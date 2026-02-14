103164797a20646eafc93b10d69fa4a8f07ee5b7
alina <connorfromcyberlife28@mail.ru>
2026-02-14 03:31:49 +0300

    gav1

--- docs/we.js
+++ docs/we.js
@@ -1,11 +1,63 @@
 /******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	// The require scope
+/******/ 	var __webpack_require__ = {};
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/global */
+/******/ 	(() => {
+/******/ 		__webpack_require__.g = (function() {
+/******/ 			if (typeof globalThis === 'object') return globalThis;
+/******/ 			try {
+/******/ 				return this || new Function('return this')();
+/******/ 			} catch (e) {
+/******/ 				if (typeof window === 'object') return window;
+/******/ 			}
+/******/ 		})();
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/publicPath */
+/******/ 	(() => {
+/******/ 		var scriptUrl;
+/******/ 		if (__webpack_require__.g.importScripts) scriptUrl = __webpack_require__.g.location + "";
+/******/ 		var document = __webpack_require__.g.document;
+/******/ 		if (!scriptUrl && document) {
+/******/ 			if (document.currentScript && document.currentScript.tagName.toUpperCase() === 'SCRIPT')
+/******/ 				scriptUrl = document.currentScript.src;
+/******/ 			if (!scriptUrl) {
+/******/ 				var scripts = document.getElementsByTagName("script");
+/******/ 				if(scripts.length) {
+/******/ 					var i = scripts.length - 1;
+/******/ 					while (i > -1 && (!scriptUrl || !/^http(s?):/.test(scriptUrl))) scriptUrl = scripts[i--].src;
+/******/ 				}
+/******/ 			}
+/******/ 		}
+/******/ 		// When supporting browsers where an automatic publicPath is not supported you must specify an output.publicPath manually via configuration
+/******/ 		// or pass an empty string ("") and set the __webpack_public_path__ variable from your code to use your own logic.
+/******/ 		if (!scriptUrl) throw new Error("Automatic publicPath is not supported in this browser");
+/******/ 		scriptUrl = scriptUrl.replace(/^blob:/, "").replace(/#.*$/, "").replace(/\?.*$/, "").replace(/\/[^\/]+$/, "/");
+/******/ 		__webpack_require__.p = scriptUrl;
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+
+;// ./src/img/we1.svg
+const we1_namespaceObject = __webpack_require__.p + "images/5ef55ed8a0bb29ef6d96.svg";
+;// ./src/img/we2.svg
+const we2_namespaceObject = __webpack_require__.p + "images/97177a3da8414fc7326c.svg";
+;// ./src/img/we3.svg
+const we3_namespaceObject = __webpack_require__.p + "images/be5a6e698409f03496c8.svg";
+;// ./src/js/we.js
+
+
+
 var img = document.querySelector('.weimg');
 var but = document.querySelector('.like');
-var images = ['../img/we1.svg', '../img/we2.svg', '../img/we3.svg'];
+var we_images = [we1_namespaceObject, we2_namespaceObject, we3_namespaceObject];
 var index = 0;
 but.addEventListener('click', function () {
-  index = (index + 1) % images.length;
-  img.src = images[index];
+  index = (index + 1) % we_images.length;
+  img.src = we_images[index];
 });
 /******/ })()
 ;
